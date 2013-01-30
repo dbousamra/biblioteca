5e4ca1e4914d608e1e2ec21510c642cc68196c7b
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-01-30 10:05:04 +1000

    Removed unused BookNotFoundExceptions

--- src/com/twu/biblioteca/items/BookNotFoundException.java
+++ src/com/twu/biblioteca/items/BookNotFoundException.java
@@ -1,20 +1,7 @@
 package com.twu.biblioteca.items;
 
 public class BookNotFoundException extends Exception {
-
-    public BookNotFoundException() {
-        super();
-    }
-
     public BookNotFoundException(String isbn) {
         super("Book with ISBN: " + isbn + " not found in library.");
-    }
-
-    public BookNotFoundException(String isbn, Throwable cause) {
-        super("Book with ISBN: " + isbn + " not found in library.", cause);
-    }
-
-    public BookNotFoundException(Throwable cause) {
-        super(cause);
     }
 }
