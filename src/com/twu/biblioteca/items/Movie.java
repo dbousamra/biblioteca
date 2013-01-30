6830a1a594b9b18aa9a75f5964bfbff2e694ff88
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-01-30 10:46:38 +1000

    Overridden equals and hashcode in order for the manager to work, since I am using a Set<Movie>

--- src/com/twu/biblioteca/items/Movie.java
+++ src/com/twu/biblioteca/items/Movie.java
@@ -28,4 +28,28 @@ public class Movie {
     public boolean isRated() {
         return this.rated;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+
+        if (rated != movie.rated) return false;
+        if (!director.equals(movie.director)) return false;
+        if (!title.equals(movie.title)) return false;
+        if (!year.equals(movie.year)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + year.hashCode();
+        result = 31 * result + director.hashCode();
+        result = 31 * result + (rated ? 1 : 0);
+        return result;
+    }
 }
