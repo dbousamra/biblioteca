f8ab99d5a1c31fdf1d6e0e6f38262bbc320f4050
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-02-01 10:16:52 +1000

    User class modified to correct tests

--- src/com/twu/biblioteca/users/User.java
+++ src/com/twu/biblioteca/users/User.java
@@ -1,9 +1,25 @@
 package com.twu.biblioteca.users;
 
 public class User {
-    public User() { } //blank for the moment.
+
+    private final String username;
+    private final String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
     public String checkMembershipDetails() {
         return "Please talk to a Librarian. Thank you.";
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+
+        return username;
     }
 }
