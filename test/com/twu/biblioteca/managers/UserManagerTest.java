78c7e7564c338702529542646d379210aed1fd67
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-02-01 10:24:16 +1000

    First couple of tests for new User functionality

---
+++ test/com/twu/biblioteca/managers/UserManagerTest.java
@@ -0,0 +1,27 @@
+package com.twu.biblioteca.managers;
+
+import com.twu.biblioteca.users.User;
+import com.twu.biblioteca.users.UserManager;
+import org.junit.Test;
+
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+public class UserManagerTest {
+    @Test
+    public void testUserManagerCreation() throws Exception {
+        UserManager userManager = new UserManager();
+        assertNotNull(userManager);
+    }
+
+    @Test
+    public void testAddRegisteredUsers() throws Exception {
+        UserManager userManager = new UserManager();
+        User user = new User("111-1111", "somePassword");
+        userManager.addUser(user);
+        assertThat(userManager.getListOfUsers(), hasItem(user));
+    }
+
+
+}
